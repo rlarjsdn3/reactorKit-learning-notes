d80fce9bd94e7050624f89e58f6f71be21c6893f
Zizi_Kim <rlarjsdn3@naver.com>
2023-11-24 15:23:35 +0900

    commit

---
+++ HelloReactorKit/Counter/Counter/CounterViewController.swift
@@ -0,0 +1,93 @@
+//
+//  CounterViewController.swift
+//  HelloReactorKit
+//
+//  Created by 김건우 on 11/23/23.
+//
+
+import UIKit
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+
+final class CounterViewController: UIViewController, StoryboardView {
+    @IBOutlet weak var increaseButton: UIButton!
+    @IBOutlet weak var decreaseButton: UIButton!
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    let counterViewReactor: CounterViewReactor = CounterViewReactor()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind(reactor: counterViewReactor)
+    }
+    
+    func bind(reactor: CounterViewReactor) {
+        // Action
+        increaseButton.rx.tap
+            .map { CounterViewReactor.Action.increase }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        decreaseButton.rx.tap
+            .map { CounterViewReactor.Action.decrease }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        // State
+        reactor.state.map { $0.value }
+            .distinctUntilChanged()
+            .map { "\($0)" }
+            .bind(to: valueLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { !$0.isLoading }
+            .distinctUntilChanged()
+            .bind(to: activityIndicatorView.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.isLoading }
+            .distinctUntilChanged()
+            .bind(to: activityIndicatorView.rx.isAnimating)
+            .disposed(by: disposeBag)
+        
+        reactor.pulse(\.$alertMessage)
+            .debug("[Pulse Event]")
+            .compactMap{ $0 }
+            .subscribe(onNext: { [weak self] message in
+                let alertController = UIAlertController(
+                  title: nil,
+                  message: message,
+                  preferredStyle: .alert
+                )
+                alertController.addAction(UIAlertAction(
+                  title: "OK",
+                  style: .default,
+                  handler: nil
+                ))
+                self?.present(alertController, animated: true)
+            })
+            .disposed(by: disposeBag)
+        
+//        reactor.state.map { $0.alertMessage }
+//            .compactMap { $0 }
+//            .distinctUntilChanged()
+//            .subscribe(onNext: { [weak self] message in
+//                let alertController = UIAlertController(
+//                  title: nil,
+//                  message: message,
+//                  preferredStyle: .alert
+//                )
+//                alertController.addAction(UIAlertAction(
+//                  title: "OK",
+//                  style: .default,
+//                  handler: nil
+//                ))
+//                self?.present(alertController, animated: true)
+//            })
+//            .disposed(by: disposeBag)
+    }
+}
